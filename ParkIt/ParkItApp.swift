627bc0f32fffac79486672f311b9c2db2c506a71
aslamroh <rohan.aslam@mail.utoronto.ca>
2024-11-18 16:58:59 -0500

    Initial Commit

---
+++ ParkIt/ParkItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkItApp.swift
+//  ParkIt
+//
+//  Created by Rohan Aslam on 2024-11-18.
+//
+
+import SwiftUI
+
+@main
+struct ParkItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParkItTests/ParkItTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParkItTests.swift
+//  ParkItTests
+//
+//  Created by Rohan Aslam on 2024-11-18.
+//
+
+import Testing
+@testable import ParkIt
+
+struct ParkItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
